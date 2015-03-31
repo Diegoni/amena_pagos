58a17e0623e7338b0afc15761508c73019dd26d3
Diegoni <diego_nieto_1@hotmail.com>
2015-03-31 16:38:39 -0300

    1.1.26

--- helpers/h_language.php
+++ helpers/h_language.php
@@ -1,36 +1,61 @@
 <?php
-$language['certificado']				= 'Certificado';
-$language['guardar']					= 'Guardar';
-$language['volver']						= 'Volver';
-$language['update_ok']					= 'El registro se ha modificado con éxito';
-$language['config']						= 'Config';
-$language['transacciones']				= 'Transacciones';
-$language['reporte']					= 'Reporte';
-$language['generar_informe']			= 'Generar informe de interbanking ';
-$language['info_informe']				= 'El siguiente link muestra el reporte de transferencia, previo Login';
-$language['info_estados']				= 'El siguiente link el estado de transferencias, previo Login';
-$language['estado']						= 'Estado';
-$language['usuario']					= 'Usuario';
-$language['clave']						= 'Clave';
-$language['cuil']						= 'Cuil';
-$language['comunidad']					= 'Comunidad';
-$language['mensaje_enviado']			= 'Mensaje enviado, la administración se comunicara con usted.';
-$language['no_ingreso']					= 'No puedes ingresar.';
-$language['no_acceso']					= 'Problemas para acceder.';
-$language['cerrar']						= 'Cerrar';
-$language['mensaje']					= 'Mensaje';
-$language['email']						= 'Email';
-$language['enviar']						= 'Enviar';
-$language['ingrese']					= 'Ingrese';
-$language['reintentar']					= 'Reintentar';
-$language['usuario_error']				= 'El usuario o el password no han sido ingresados.';
-$language['usuario_delete']				= 'Error, su usuario ha sido eliminado';
-$language['no_connect']					= 'Error, no se ha podido conectar';
-$language['ir_arriba']					= 'Ir Arriba';
-$language['desarrollado']				= 'Desarrollado por TMS GROUP SA';
-$language['salir']						= 'Salir';
-$language['ingresar']					= 'Ingresar';
-$language['password']					= 'Password';
-$language['post']						= 'Post';
-$language['url']						= 'Url';
+// C
+$language['cerrar']						= 'Cerrar';	
+$language['certificado']				= 'Certificado';			
+$language['clave']						= 'Clave';	
+$language['comunidad']					= 'Comunidad';		
+$language['config']						= 'Config';	
+$language['cuil']						= 'Cuil';	
+
+// D
+$language['desarrollado']				= 'Desarrollado por TMS GROUP SA';	
+
+// E		
+$language['email']						= 'Email';	
+$language['enviar']						= 'Enviar';	
+$language['estado']						= 'Estado';	
+
+// G
+$language['generar_informe']			= 'Generar informe de interbanking ';				
+$language['guardar']					= 'Guardar';	
+
+// I	
 $language['id']							= 'ID';
+$language['info_estados']				= 'El siguiente link el estado de transferencias, previo Login';			
+$language['info_informe']				= 'El siguiente link muestra el reporte de transferencia, previo Login';			
+$language['ingresar']					= 'Ingresar';		
+$language['ingrese']					= 'Ingrese';		
+$language['ir_arriba']					= 'Ir Arriba';	
+
+// M	
+$language['mensaje']					= 'Mensaje';		
+$language['mensaje_enviado']			= 'Mensaje enviado, la administración se comunicara con usted.';
+
+// N				
+$language['no_acceso']					= 'Problemas para acceder.';		
+$language['no_connect']					= 'Error, no se ha podido conectar';		
+$language['no_ingreso']					= 'No puedes ingresar.';
+
+// P	
+$language['password']					= 'Password';		
+$language['post']						= 'Post';
+
+// R	
+$language['reintentar']					= 'Reintentar';		
+$language['reporte']					= 'Reporte';
+
+// S		
+$language['salir']						= 'Salir';
+
+// T	
+$language['transacciones']				= 'Transacciones';
+
+// U			
+$language['update_ok']					= 'El registro se ha modificado con éxito';		
+$language['url']						= 'Url';	
+$language['usuario']					= 'Usuario';		
+$language['usuario_delete']				= 'Error, su usuario ha sido eliminado';			
+$language['usuario_error']				= 'El usuario o el password no han sido ingresados.';
+
+// V			
+$language['volver']						= 'Volver';	
